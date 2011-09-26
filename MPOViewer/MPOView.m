a379182b540a21de7b70d256ffd41aacd1be8b75
Karl Baron <github@kalleboo.com>
2011-09-26 14:21:33 +0900

    code cleanup

--- MPOViewer/MPOView.m
+++ MPOViewer/MPOView.m
@@ -12,55 +12,25 @@
 @synthesize image;
 @synthesize offset;
 
-
-static CGColorRef CGColorCreateFromNSColor (CGColorSpaceRef colorSpace, NSColor *color)
-{
-    NSColor *deviceColor = [color colorUsingColorSpaceName:NSDeviceRGBColorSpace];
-    
-    CGFloat components[4];
-    [deviceColor getRed: &components[0] green: &components[1] blue:
-     &components[2] alpha: &components[3]];
-    
-    return CGColorCreate (colorSpace, components);
-}
-
 - (void)drawRect:(NSRect)dirtyRect
 {
     CGFloat width = self.bounds.size.width;
     
-    CGContextRef context = (CGContextRef)[[NSGraphicsContext currentContext] graphicsPort];
-    
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    /*
-    CGColorRef cgColor = CGColorCreateFromNSColor(colorSpace, [NSColor clearColor]);
-    
-    CGContextSetFillColorWithColor(context, cgColor);
-    CGContextSetRGBFillColor(context, 1, 0, 0, 1);
-    CGContextSetBlendMode(context, kCGBlendModeClear);
-    CGContextFillRect(context, self.bounds);
-    
-    CGContextSetBlendMode(context, kCGBlendModeNormal);
-     */
-    
     if (!cached && image) {
-        NSLog(@"caching...");
-        NSLog(@"%f x %f",image.size.width,image.size.height);
+        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
         NSRect r = NSRectFromCGRect(CGRectMake(0, 0, image.size.width, image.size.height));
+        
         CGContextRef context2 = CGBitmapContextCreate(NULL, image.size.width, image.size.height, 8, image.size.width*4, colorSpace, kCGImageAlphaPremultipliedLast);
+        
         CGContextDrawImage(context2, r, [[[image representations] objectAtIndex:offset] CGImageForProposedRect:&r context:nil hints:nil]);
+
         cached = CGBitmapContextCreateImage(context2);
+        
+        CGColorSpaceRelease(colorSpace);
     }
     
+    CGContextRef context = (CGContextRef)[[NSGraphicsContext currentContext] graphicsPort];
     
-//    NSRect r = self.bounds;
-    
-    NSLog(@"%@",image);
-//    [image drawRepresentation:[image.representations objectAtIndex:offset] inRect:self.bounds];
- //       CGContextDrawImage(context2, self.bounds, [image CGImageForProposedRect:&r context:NULL hints:nil]);
-
-//    [image drawInRect:[self bounds]];
-    NSLog(@"drew %d %f",offset,self.bounds.size.height);
-
     CGContextBeginPath(context);
     for (CGFloat y = offset; y<self.bounds.size.height; y+=2) {
         CGContextMoveToPoint(context, 0, y);
@@ -71,11 +41,6 @@ static CGColorRef CGColorCreateFromNSColor (CGColorSpaceRef colorSpace, NSColor 
     CGContextClip(context);
     
     CGContextDrawImage(context, CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height), cached);
-    
-    CGColorSpaceRelease(colorSpace);
-    /*
-    CGColorRelease (cgColor);
-     */
 }
 
 @end
--- MPOViewer/MPOViewerAppDelegate.m
+++ MPOViewer/MPOViewerAppDelegate.m
@@ -21,16 +21,10 @@
     NSString* imageName = [[NSBundle mainBundle]
                            pathForResource:@"test" ofType:@"mpo"];
     NSImage* test = [[NSImage alloc] initWithContentsOfFile:imageName];
-    [test setCacheMode:NSImageCacheNever];
 
-    NSRect r = NSRectFromCGRect(CGRectMake(0, 0, test.size.width, test.size.height));
-//    view1.image = [[[test representations] objectAtIndex:0] CGImageForProposedRect:&r context:nil hints:nil];
-//    view1.image = [[test representations] objectAtIndex:0];
     view1.image = test;
     view1.offset = 0;
     
-//    view2.image = [[[test representations] objectAtIndex:1] CGImageForProposedRect:&r context:nil hints:nil];
-//    view2.image = [[test representations] objectAtIndex:1];
     view2.image = test;
     view2.offset = 1;
     
